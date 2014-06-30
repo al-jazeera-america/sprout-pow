b2f1dadce6553aa0a4780ac650c1c8b79b714585
Glenn Sidney <glenn@glennfu.com>
2014-06-30 12:54:50 -0500

    Trying without specifying user, it didn't ask for password and it should have

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'ajam-dev@aljazeera.net'
 license          'MIT'
 description      'Installs/Configures pow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'mac_os_x'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,5 @@
 run_unless_marker_file_exists("pow") do
   execute "curl get.pow.cx | sh" do
-    user node['current_user']
     environment( { 'HOME' => node['sprout']['home'] } )
   end
 end
